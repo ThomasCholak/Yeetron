72ff6f48f7b8e47938bd6a47852e2906a84a964d
Thomas Cholak <63080803+ThomasCholak@users.noreply.github.com>
2023-12-29 15:25:10 -0600

    Create app.js

---
+++ app.js
@@ -0,0 +1,104 @@
+document.addEventListener('DOMContentLoaded', function ()
+{
+    start_str();
+    yeet_anim();
+
+    if (/Mobi|Android/i.test(navigator.userAgent)) {
+        // Code to execute on a mobile browser
+        console.log("This code is running on a mobile browser");
+      } else {
+        // Code for non-mobile browsers
+        console.log("This code is running on a non-mobile browser");
+      }
+});
+
+function start_str()  // loading screen
+{
+    let myString = "L";
+
+    setTimeout(function() {
+        myString += "o";
+        document.body.innerHTML = myString;
+    }, 50);
+
+    setTimeout(function() {
+        myString += "a";
+        document.body.innerHTML = myString;
+    }, 100);
+
+    setTimeout(function() {
+        myString += "d";
+        document.body.innerHTML = myString;
+    }, 110);
+    setTimeout(function() {
+        myString += "i";
+        document.body.innerHTML = myString;
+    }, 150);
+
+    setTimeout(function() {
+        myString += "n";
+        document.body.innerHTML = myString;
+    }, 160);
+
+    setTimeout(function() {
+        myString += "g";
+        document.body.innerHTML = myString;
+    }, 200);
+
+    setTimeout(function() {
+        myString += ".";
+        document.body.innerHTML = myString;
+    }, 300);
+
+    setTimeout(function() {
+        myString += ".";
+        document.body.innerHTML = myString;
+    }, 350);
+
+    setTimeout(function() {
+        myString += ".";
+        document.body.innerHTML = myString;
+    }, 399);
+}
+  
+function yeet_anim()  // sets the frames of the animation
+{
+    var yeet_str1 = "<br>" +
+    " _   _   _____   _____   _______   _____    _____    ____   __<br>" +
+    "| | | | |  ___| |  ___| |       | |     \\  |     |  |    \\ |  |<br>" +
+    "|_   _| | |___  | |___   ￣| |￣  | |￣| | | |￣| |  |     \\|  |<br>" +
+    "  | |   |  ___| |  ___|    | |    |  ￣ /  | | | |  |         |<br>" +
+    "  | |   | |___  | |___     | |    | |\\ \\   | |_| |  |  |\\     |<br>" +
+    "  | |   |     | |     |    | |    | | \\ \\  |     |  |  | \\    |<br>" +
+    "   ￣    ￣￣￣   ￣￣￣     ￣     ￣   ￣   ￣￣￣   ￣￣  ￣￣";
+
+    var yeet_str2 = "<br><br>" +
+    " _   _   _____   _____   _______   _____    _____    ____   __<br>" +
+    "| | | | |  ___| |  ___| |       | |     \\  |     |  |    \\ |  |<br>" +
+    "|_   _| | |___  | |___   ￣| |￣  | |￣| | | |￣| |  |     \\|  |<br>" +
+    "  | |   |  ___| |  ___|    | |    |  ￣ /  | | | |  |         |<br>" +
+    "  | |   | |___  | |___     | |    | |\\ \\   | |_| |  |  |\\     |<br>" +
+    "  | |   |     | |     |    | |    | | \\ \\  |     |  |  | \\    |<br>" +
+    "   ￣    ￣￣￣   ￣￣￣     ￣     ￣   ￣   ￣￣￣   ￣￣  ￣￣";
+
+    var currentIndex = 0;
+    var texts = [yeet_str1, yeet_str2];
+  
+    setInterval(function ()
+    {
+      displayText(texts[currentIndex]);
+      currentIndex = (currentIndex + 1) % texts.length;
+    }, 400);
+}
+  
+function displayText(text)
+{
+    clearConsole();
+    var console_str = '<pre>' + text + '</pre>';
+    document.body.innerHTML = console_str;
+}
+  
+function clearConsole()  // function to clear the console
+{
+    document.body.innerHTML = '';
+}
